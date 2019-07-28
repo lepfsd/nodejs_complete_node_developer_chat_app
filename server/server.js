adb68d15d333e37f6cfd0e2aa9c5022e4c31b49c
lepfsd-strapp <lperez@master.inmobilia.com>
2019-07-28 01:48:31 -0400

    socketio

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+	socket.on('disconnect', () => {
+		console.log('uer was disconnected');
+	});
+});
+
 server.listen(4000, () => {
 	console.log(`server is up on ${port} port`);
 });
