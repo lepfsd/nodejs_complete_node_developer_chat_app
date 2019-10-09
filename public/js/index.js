c4b9f3f7ba507d3d12126279d6e4d1e7d61daf14
lepfsd-strapp <lperez@master.inmobilia.com>
2019-10-08 22:46:43 -0400

    to get location

--- public/js/index.js
+++ public/js/index.js
@@ -14,11 +14,46 @@ socket.on('connect', () => {
 	});
 });	
 
+socket.on('createLocationMessage', (coords) => {
+	io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+});
+
 socket.on('disconnect', () => {
 	console.log('disconnect from server');
 });
 
 socket.on('newMessage', function(message) {
 	console.log('New message', message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+	jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e){
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		form: 'User',
+		text: jQuery(`[name=message]`).val()
+	}, function() {
+
+	});
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+	if(!navigator.geolocation) {
+		return alert("geolocation not supported by your browser");
+	}
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+		//console.log(position);
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	}, function(){ 
+		alert("unable to fetch location");
+	});
 });
 
